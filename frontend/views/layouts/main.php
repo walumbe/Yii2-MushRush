7bb0aaa0af6c73acb2d5dd43f49c19508ec4f278
walumbe <nathanwalumbe.com>
2021-11-17 02:12:52 +0300

    fix footer layout

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -85,8 +85,15 @@ AppAsset::register($this);
 
 <footer class="footer mt-auto py-3 text-muted">
     <div class="container">
-        <p class="float-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-        <p class="float-right"><?= Yii::powered() ?></p>
+     <div class="row">
+         <div class="col">
+             <p class="float-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+         </div>
+         <div class="col text-right">
+             <p class="float-right">Created By <a href="https://github.com/walumbe" target="_blank">Walumbe</a> </p>
+         </div>
+
+     </div>
     </div>
 </footer>
 
