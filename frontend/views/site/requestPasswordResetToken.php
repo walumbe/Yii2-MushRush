6b7035f0ce57c9e64de88db45439fb54f7873279
walumbe <nathanwalumbe.com>
2021-11-17 01:14:55 +0300

    fix bootstrap 3 bugs in  site views

--- frontend/views/site/requestPasswordResetToken.php
+++ frontend/views/site/requestPasswordResetToken.php
@@ -11,12 +11,14 @@ $this->title = 'Request password reset';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-request-password-reset">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out your email. A link to reset password will be sent there.</p>
-
-    <div class="row">
+    <div class="row justify-content-center">
         <div class="col-lg-5">
+
+            <h1><?= Html::encode($this->title) ?></h1>
+
+            <p>Please fill out your email. A link to reset password will be sent there.</p>
+
             <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form']); ?>
 
                 <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
--- frontend/views/site/resendVerificationEmail.php
+++ frontend/views/site/resendVerificationEmail.php
@@ -11,12 +11,13 @@ $this->title = 'Resend verification email';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-resend-verification-email">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out your email. A verification email will be sent there.</p>
 
-    <div class="row">
+    <div class="row justify-content-center">
         <div class="col-lg-5">
+            <h1><?= Html::encode($this->title) ?></h1>
+
+            <p>Please fill out your email. A verification email will be sent there.</p>
             <?php $form = ActiveForm::begin(['id' => 'resend-verification-email-form']); ?>
 
             <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
